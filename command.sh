a1a77c2ef4915ba6e2f4149322f92dc8b2cad37a
MrtnHt <jouw-email-van-mrtnht@example.com>
2026-01-22 10:10:10 +0000

    Commando uitgevoerd

--- command.sh
+++
@@ -1,8 +0,0 @@
-# Maak de mappen aan, fix de git repo en haal de nieuwste code op
-proot-distro login ubuntu -- bash -c "
-mkdir -p /root/public_site && 
-cd /root/public_site && 
-git init && 
-git remote add origin git@github.com:MrtnHt/phoenix-projects.git || true && 
-git pull origin main && 
-echo '✅ Telefoon mappen hersteld en gesynchroniseerd!'"
